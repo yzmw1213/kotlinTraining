45f1dc48caae7a6ade2b9eff3e63df8bc7464a57
AyumuShigeta <faliplvsg@gmail.com>
2022-03-07 05:11:42 +0900

    first commit

---
+++ src/main/kotlin/com/example/tutorial/AppController.kt
@@ -0,0 +1,9 @@
+package com.example.tutorial
+
+import com.example.tutorial.training.*
+
+fun main(args: Array<String>) {
+    val name = readLine()
+    println("Hello,$name")
+    powNumber()
+}
---
+++ src/main/kotlin/com/example/tutorial/training/Number.kt
@@ -0,0 +1,21 @@
+package com.example.tutorial.training
+
+import kotlin.math.*
+
+
+fun typeNumber() {
+    val doubleNumber: Double = E
+    val floatNumber: Float
+    val num1: Float = 0.222f
+    floatNumber = log10(num1)
+
+    val longNumber: Long = 10000000000000
+    val intNumber: Int = 3
+}
+
+fun powNumber() {
+    val base: Double = 2.5
+    val exponent: Double = 2.001
+    val result = base.pow(exponent)
+    println(result)
+}
