289a03964512e1e4cf237d34877e8316d6cc7c69
AyumuShigeta <faliplvsg@gmail.com>
2022-03-08 14:40:13 +0900

    cache

--- src/main/kotlin/com/example/tutorial/AppController.kt
+++ src/main/kotlin/com/example/tutorial/AppController.kt
@@ -3,7 +3,5 @@ package com.example.tutorial
 import com.example.tutorial.training.*
 
 fun main(args: Array<String>) {
-    val name = readLine()
-    println("Hello,$name")
-    powNumber()
+    boxing()
 }
